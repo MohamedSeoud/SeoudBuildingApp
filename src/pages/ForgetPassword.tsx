6ae695817dac2eed4c90767dd856f5c69fafc24d
Mohammed Seoud <m.seoud42@gmail.com>
2023-09-07 03:57:04 +0300

    commit

--- src/pages/ForgetPassword.tsx
+++ src/pages/ForgetPassword.tsx
@@ -1,8 +1,69 @@
-import React from 'react'
+import React from 'react' 
+import image from '../assets/20944201.jpg'
+import { Icon } from '@iconify/react';
+import { Link } from 'react-router-dom';
+import {  SING_IN_PATH, SING_UP_PATH } from '../helper/navigationPath';
+
 
 function ForgetPassword() {
   return (
-    <div>
+    <div className=' flex flex-col gap-5  h-fit py-28 w-[100%] px-[30px] overflow-hidden bg-[#d4d4d4]'>
+      <div className=' uppercase text-black text-center w-[100%]  overflow-hidden text-5xl font-bold'>
+        Register
+      </div>
+      <div className=' grid grid-cols-2 max-md:grid-cols-1 gap-5 w-[100%]  my-7  overflow-hidden '>
+
+        <div className='w-[100%] col-span-1 mx-4  overflow-hidden  '>
+          <img src={image} className='rounded-[20px] h-[500px] w-[90%] '/>
+
+        </div>
+
+        <div className='flex flex-col col-span-1 py-5 w-[100%]  justify-center  overflow-hidden gap-y-6'>
+
+          <input type='text' placeholder='Email address' className='w-[100%] text-2xl px-3 h-14'/>
+
+
+
+          <div className='flex flex-row justify-between'>
+            <div>
+          <p className=' text-2xl'>Do have an account? <Link to={SING_UP_PATH} className=' text-red-600 cursor-pointer
+           hover:border-b-2
+           border-red-600 border-spacing-2'>Sign up</Link></p>
+           </div>
+           <Link to={SING_IN_PATH} className=' text-2xl max-h-[31px] text-blue-600 cursor-pointer hover:border-b-2  border-spacing-2
+            border-blue-600'>
+            Sign in instead?
+           </Link>
+           </div> 
+           <div className='w-[100%] '>
+            <button className=' uppercase text-2xl px-3 h-14 hover:bg-blue-400 bg-blue-600 w-[100%] text-center text-white my-2 '>
+              send rest password </button>
+           </div>
+           <div className='w-[100%] justify-between items-center flex flex-row '>
+            <div className='w-[45%]  bg-gray-400 h-[1.5px]'></div>
+            <div className='w-[10%]  uppercase  text-center font-bold text-xl'> OR</div>
+
+            <div className='w-[45%]  bg-gray-400 h-[1.5px]'></div>
+
+            
+           </div>
+
+           <div className='w-[100%] '>
+            <button className=' uppercase hover:bg-red-300 text-2xl flex flex-row justify-center items-center  px-3 h-14 bg-red-400 w-[100%] text-center text-white my-2 '>
+              
+              continue with google
+              <Icon icon="flat-color-icons:google" width="50" height="50" />
+              </button>
+           </div>
+
+
+        </div>
+        <div>
+
+        </div>
+
+      </div>
+
       
     </div>
   )
