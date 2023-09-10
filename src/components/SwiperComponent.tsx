eb8c06ce0c3bdd4c859ddcabb68f4567499dad00
Mohammed Seoud <m.seoud42@gmail.com>
2023-09-11 01:19:58 +0300

    cc

--- src/components/SwiperComponent.tsx
+++ src/components/SwiperComponent.tsx
@@ -13,10 +13,20 @@ import {
     Pagination
 } from "swiper/modules";
 import 'swiper/css/bundle';
+import {  FaShare } from 'react-icons/fa';
+import { useState } from 'react';
 
 function SwiperComponent() {
     const imgArr=[image1,image2,image3,image4,image5,image6];
+    const[copied,setCopied] =useState(false)
     SwiperCore.use([EffectFade,Autoplay,Navigation,Pagination])
+
+    const onCopyHandler = ()=>{
+        navigator.clipboard.writeText(window.location.href)
+        setCopied(true)
+        setTimeout(()=> setCopied(false),2000)
+    }
+
   return (
     <>
     <Swiper slidesPerView={1} navigation pagination={{type:"progressbar"}} effect='fade' 
@@ -26,7 +36,20 @@ function SwiperComponent() {
                 <SwiperSlide key={index}>
                     <div className=' overflow-hidden h-[400px] w-full' 
                     style={{background:`url(${url}) center no-repeat`, backgroundSize:'cover'}}>
+
+                    <div className=' flex  flex-col  gap-4  justify-center items-end p-12  text-gray-500'>
+                        <span onClick={onCopyHandler} className=' h-16 w-16 rounded-full cursor-pointer bg-white flex justify-center items-center '>
+                         <FaShare size="2.5rem"/>
+                        </span>
+                        {   copied &&
+                            <span className=' bg-white p-2 rounded-lg text-black  font-bold text-3xl text-center'>
+                             copied!
+                            </span>
+                        }
+
                     </div>
+                </div>
+
 
                 </SwiperSlide>
             ))
