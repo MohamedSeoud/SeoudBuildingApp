944e9a7ec4483bee2a76836d95d17e87aa205c8c
Mohammed Seoud <m.seoud42@gmail.com>
2023-09-11 19:13:08 +0300

    xx

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
 
-  },
   build: {
     chunkSizeWarningLimit: 100000000
   },
