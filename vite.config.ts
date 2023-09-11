59b60437bb6f3fb7674d17af473f4bc6467ee0ef
Mohammed Seoud <m.seoud42@gmail.com>
2023-09-11 19:11:45 +0300

    cc

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,4 @@
 import { defineConfig } from 'vite'
-import { esbuildCommonjs } from '@originjs/vite-plugin-commonjs';
 
 import react from '@vitejs/plugin-react-swc'
 
@@ -7,9 +6,7 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
-    esbuildOptions: {
-      plugins: [esbuildCommonjs(['react-moment'])],
-    },
+
   },
   build: {
     chunkSizeWarningLimit: 100000000
